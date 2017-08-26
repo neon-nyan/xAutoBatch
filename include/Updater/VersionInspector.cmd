15b1b7eed40355929f57edb54eadb4d30941b5f5
neon-nyan <dev.kemalsetyaa@gmail.com>
2017-08-26 21:31:57 +0700

    more comments :3

--- include/Updater/VersionInspector.cmd
+++ include/Updater/VersionInspector.cmd
@@ -9,11 +9,12 @@
     echo Memeriksa Update...
     "%wgetPath%" --no-check-certificate -q -O "%temp%\UPDATE" https://raw.githubusercontent.com/neon-nyan/xAutoBatch/master/UPDATE
 
+    REM Bila errorlevel == 1, maka munculkan pesan error.
     if "%errorlevel%" GEQ "1" (
         cls
         title=Update - ERROR
         echo %debugStat%Terjadi kesalahan dalam proses pembaruan...
-        echo Jaringan terganggu. Silahkan periksa kembali koneksi Internet anda. :3
+        timeout /t 3 /nobreak | echo Jaringan terganggu. Silahkan periksa kembali koneksi Internet anda. :3
         goto :__end
     )
 
@@ -30,12 +31,25 @@ REM             )
 REM         )
 REM     )
 
+REM De-eliminasi data RAW yang ada dalam file Chunk untuk update.
+REM dengan struktur data RAW:
+REM     [codename]_v[ver_xxx].r[rev_x]
+REM contoh:
+REM     Kuvukiland_v696.r9
 :GETUpdateNewVersion
     setlocal enabledelayedexpansion
+    REM De-eliminasi data menjadi dua bagian, contoh:
+    REM     1: Kuvukiland
+    REM     2: v696.r9
     for /f "tokens=1,2 delims=_" %%a in ('echo %NewVerUpdate%') do (
+        REM Tetapkan data variable newcodename dengan bagian ke-1.
         set newcodename=%%a
+        REM De-eliminasi data variable ke-2 menjadi dua bagian:
+        REM     1: 696
+        REM     2: 9
         for /f "tokens=1 delims=v" %%a in ('echo %%b') do (
             for /f "tokens=1,2 delims=.r" %%a in ('echo %%a') do (
+                REM Tetapkan data variable newver "696", newrev "9", dan newallver "newver+newrev=6969"
                 set newver=%%a
                 set newrev=%%b
                 set newallver=%%a%%b
@@ -44,7 +58,10 @@ REM     )
     )
 
 :CheckCurVer
-REM set oldver=%majver%%minver%%revver%&&if not "!newver!" GTR "!oldver!" goto :__end
+    REM Tetapkan data variable oldver dengan versi internal.
+    REM Bila newallver tidak lebih dari oldver, maka akhiri sesi.
+    REM Bila newallver lebih dari oldver, maka lanjutkan sesi.
+    REM Bila newallver sama dengan oldver, maka nyatakan bahwa versi yang digunakan sama atau terbaru.
     set oldver=%ver%%rev%&&if not "!newallver!" GTR "!oldver!" (
         goto :__end
     ) else if "!newallver!" == "!oldver!" (
@@ -52,7 +69,7 @@ REM set oldver=%majver%%minver%%revver%&&if not "!newver!" GTR "!oldver!" goto :
         cls
         echo Versi yang anda gunakan adalah yang terbaru.
         echo.
-        echo Versi saat ini: !newver!
+        timeout /t 3 /nobreak | echo Versi saat ini: !newver!
         goto :__end
     )
 
@@ -75,12 +92,31 @@ REM set oldver=%majver%%minver%%revver%&&if not "!newver!" GTR "!oldver!" goto :
         goto :__end
     )
 
+REM Fetch data update dari server GitHub dengan tag dan lakukan penerapan terhadap program.
 :DoUpdate
     cls
     if /i not exist "%downtempdir%" md "%downtempdir%"
     title=Mendapatkan update...
     %wgetPath% --no-check-certificate -L -q https://github.com/neon-nyan/xAutoBatch/releases/download/v!newver!.r!newrev!/xAutoBatch-!newver!.r!newrev!.zip -O "%downtempdir%\%progname%-!newver!.r!newrev!.zip" | echo Mendapatkan paket...
+    if "%errorlevel%" GEQ "1" (
+        echo Terjadi kesalah dalam pengambilan data paket pembaruan.
+        timeout /t 3 /nobreak | echo.
+        echo Program Error  : wget
+        echo Program Path   : %wgetPath%
+        echo Time           : %date% - %time%
+        goto :__end
+    )
+
     %unzipPath% -qq -o -d "%downtempdir%" "%downtempdir%\xAutoBatch-!newver!.r!newrev!.zip" | echo Decompress paket...
+    if "%errorlevel%" GEQ "1" (
+        echo Terjadi kesalah dalam proses pen-dekompresan.
+        timeout /t 3 /nobreak | echo.
+        echo Program Error  : unzip
+        echo Program Path   : %unzipPath%
+        echo Time           : %date% - %time%
+        goto :__end
+    )
+
     xcopy /E /H /R /Y "%downtempdir%\xAutoBatch-!newver!.r!newrev!" "." | echo Terapkan paket...
     
     :StatUpdateDone
@@ -90,6 +126,7 @@ REM set oldver=%majver%%minver%%revver%&&if not "!newver!" GTR "!oldver!" goto :
 
         msg * Program telah keluar. Mohon jalankan kembali untuk melanjutkan.
 
+        REM Buka release note 
         start https://github.com/neon-nyan/xAutoBatch/releases/tag/v!newver!.r!newrev!
 
 :__end
